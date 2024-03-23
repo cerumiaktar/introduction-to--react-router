bd8d0fe034d26ebeea8459c912f541984afb9f01
rumiaktar <ce.rumiakter@gmail.com>
2024-03-24 00:48:30 +0600

    usenavigate, usenavigation, uselocation

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import './Header.css';
 
 const Header = () => {
@@ -7,9 +7,10 @@ const Header = () => {
             <span>My Website</span>
             <nav>
                 <Link to="/">Home</Link>
+                <NavLink to="/users">Users</NavLink>
                 <Link to="/about">About</Link>
                 <Link to="/contact">Contact</Link>
-                <Link to="/users">Users</Link>
+                
                 <Link to="/posts">Posts</Link>
             </nav>
         </div>
